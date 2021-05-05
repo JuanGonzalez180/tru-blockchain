b5ef516bed1e0125b247c860c25f666d85fd919b
JuanGonzalez180 <38679361+JuanGonzalez180@users.noreply.github.com>
2021-05-05 13:31:00 -0500

    Change Docs Expression Validator

--- docs/components-page-page-module.js
+++ docs/components-page-page-module.js
@@ -42,7 +42,7 @@ class ValidationsService {
     }
     static emailValidator(control) {
         // RFC 2822 compliant regex
-        if (control && control.value && control.value.match(/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/)) {
+        if (control && control.value && control.value.match(/[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}$/)) {
             return null;
         }
         else {
